3885ee9f8bb56803d2a8c97736393aef56bbbedb
HalimaSaadia <halimasadia9666@gmail.com>
2024-12-25 21:56:22 +0600

    completed query builder

--- src/app/modules/students/student.service.ts
+++ src/app/modules/students/student.service.ts
@@ -1,29 +1,35 @@
 import mongoose from "mongoose";
 import { StudentModel } from "./student.mdel";
 import { TStudent } from "./student.interface";
+import QueryBuilder from "../../builder/QueryBuilder";
 
 export const getStudentsFromDB = async (query: Record<string, unknown>) => {
-  const searchTerm = query.searchTerm || "";
-  const queryObj = { ...query };
-  const excludedFields = ["searchTerm", "limit", "page","fields"];
-  excludedFields.forEach((field) => delete queryObj[field]);
-  const searchQuery = StudentModel.find({
-    $or: ["name", "name.firstName", "name.middleName", "name.lastName"].map(
-      (field) => ({
-        [field]: { $regex: searchTerm, $options: "i" },
-      })
-    ),
-  });
-  const filterQuery = searchQuery.find(queryObj);
-  const page = query.page || 1;
-  const limit = Number(query.limit) || 1;
-  const skip = Number(page) * limit;
-  const paginateQuery = filterQuery.skip(skip);
-  const limitQuery =  paginateQuery.limit(Number(limit));
- const fields  = query?.fields ? query.fields.split(",").join(" ")  : "-__v"
+//   const searchTerm = query.searchTerm || "";
+//   const queryObj = { ...query };
+//   const excludedFields = ["searchTerm", "limit", "page","fields"];
+//   excludedFields.forEach((field) => delete queryObj[field]);
+//   const searchQuery = StudentModel.find({
+//     $or: ["name", "name.firstName", "name.middleName", "name.lastName"].map(
+//       (field) => ({
+//         [field]: { $regex: searchTerm, $options: "i" },
+//       })
+//     ),
+//   });
+//   const filterQuery = searchQuery.find(queryObj);
+//   const page = Number(query.page) || 1;
+//   const limit = Number(query.limit) || 1;
+//   const skip = (page-1) * limit
+//   const paginateQuery = filterQuery.skip(skip);
+//   const limitQuery =  paginateQuery.limit(Number(limit));
+//  const fields  = query?.fields  ? query.fields.split(",").join(" ")  : "-__v"
 
- const fieldQuery = await limitQuery.select(fields)
-  return fieldQuery;
+//  const fieldQuery = await limitQuery.select(fields)
+//   return fieldQuery;
+const studentSearchableFields = ["name", "name.firstName", "name.middleName", "name.lastName"]
+const studentQuery = new QueryBuilder(StudentModel.find(),query).search(studentSearchableFields).filter().paginate()
+const result = await studentQuery.modelQuery
+return result
+
 };
 
 export const getSingleStudentFromDB = async (id: string) => {
